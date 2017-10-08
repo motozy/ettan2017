996285b18467bd4b1a94abbc735387dffa18eb51
Motoji Nagata <nagata@uniq.jp>
2017-10-09 05:09:33 +0900

    firebaseでのtwitterログイン

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ var ettan2017 = {
 
     didLoad: function() {
         this.fxxkinApple();
+
+        var login = document.getElementById("login");
+        login.addEventListener('click', function (event) {
+            event.stopPropagation(); // 下にclickイベントが行かないように
+            this.onLogin();
+        }.bind(this));
         
         window.addEventListener('resize', function (event) {
             this.onResize();
@@ -81,6 +87,31 @@ var ettan2017 = {
         imageContainer.style.width = this.imageScale * 100 + "vw";
         imageContainer.style.height = this.imageScale * 100 + "vw";
         imageContainer.style.fontSize = this.imageScale * 0.6 + "vw";
+    },
+
+    onLogin: function() {
+        var provider = new firebase.auth.TwitterAuthProvider();
+        provider.setCustomParameters({
+            'lang': 'ja'
+        });
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+            // You can use these server side with your app's credentials to access the Twitter API.
+            var token = result.credential.accessToken;
+            var secret = result.credential.secret;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
     },
 
     fxxkinApple() {
